cac17f07d79432ca0f5060ac0362aa7ba36e563a
Artur Fiedorowicz <afiedorowicz@javonet.com>
2024-06-07 11:17:14 +0200

    Added code starters for each langauge

--- player/code-starters/dotnet/dotnet_3/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace JavonetSpeedOfIntegration
-{
-    static void Main(string[] args)
-    {
-        //Can you solve the Rubik's Cube?
-    }
-}
---
+++ player/code-starters/ruby/ruby.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Set the base folder name and path
+set "base_folder_name=ruby"
+set "base_folder=%~dp0%base_folder_name%_0"
+
+REM Get the latest folder number
+set "latest_number=0"
+for /d %%F in ("%~dp0%base_folder_name%_*") do (
+    set "folder_name=%%~nxF"
+    for /f "tokens=2 delims=_" %%A in ("!folder_name!") do (
+        set "number=%%A"
+        if !number! gtr !latest_number! set "latest_number=!number!"
+    )
+)
+
+REM Increment the latest folder number
+set /a new_number=latest_number+1
+set "new_folder_name=%base_folder_name%_%new_number%"
+set "new_folder_path=%~dp0%new_folder_name%"
+
+REM Copy the base folder to the new folder
+xcopy "%base_folder%" "%new_folder_path%" /e /i /h /k /y
+
+REM Copy the Robot client file
+copy ..\..\websocket-clients\dotnet\bin\Debug\net8.0\RobotConnector.dll "%new_folder_path%"
+
+REM Open the new folder in Visual Studio Code
+cd "%new_folder_path%"
+code main.py
+
+endlocal
