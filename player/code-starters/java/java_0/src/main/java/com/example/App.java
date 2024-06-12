d01b92a4b47f73455fdacef6d5de85c26c984315
Przemyslaw Ladynski <pladynski@sdncenter.pl>
2024-06-12 08:09:26 +0200

    simplified java.app

--- player/code-starters/java/java_0/src/main/java/com/example/App.java
+++ player/code-starters/java/java_0/src/main/java/com/example/App.java
@@ -1,10 +1,9 @@
 package com.example;
-//Add import for Javonet here:
+//IMPORT HERE ...
 
 public final class App {
+
     public static void main(String[] args) {
-        System.out.println("Lets solve rubik's cube!");
-        // Let's play a game!
-        // Paste Javonet code here:
+        //CODE HERE ...
     }
 }
